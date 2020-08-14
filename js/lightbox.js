fc36c4613b8db19048cd635f106cccc402702b6c
msanket828 <Sanketmane159@gmail.com>
2020-08-13 23:06:56 -0400

    first commit

---
+++ js/lightbox.js
@@ -0,0 +1,29 @@
+
+
+var lightbox=document.getElementById("lightbox");
+var images=document.querySelectorAll(".grid img");
+
+
+images.forEach(function(image) {
+	image.addEventListener('click',function(e) {
+		//adding new image  in lightbox....
+		var lightboxImage=document.createElement("img");
+				lightboxImage.setAttribute("class","lightboxImage");
+				lightboxImage.src=image.src;
+				//every time removing the previous selected image;
+				while(lightbox.firstChild) {					
+					lightbox.removeChild(lightbox.firstChild);
+				}
+				lightbox.appendChild(lightboxImage);
+		lightbox.classList.add("active");									
+	})
+})
+
+lightbox.addEventListener('click',function(e) {
+	// console.log(e.target);==>means where you click on
+	// console.log(e.currentTarget);==>currently click event on 
+	if(e.target==e.currentTarget) {		
+		lightbox.classList.remove("active");
+	}
+
+})
